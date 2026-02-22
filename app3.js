6e62534941d583b3f8fdc44217da6ea03f72170d
Rohaan Bhagat <rudrarohaan@gmail.com>
2026-02-23 00:48:19 +0530

    Update app3.js

--- app3.js
+++ app3.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY','BABU' ],
+			strings: [ 'HAPPY', 'BIRTHDAY','AYUSH' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
@@ -401,6 +401,7 @@ window.addEventListener( 'resize', function(){
 	ctx.font = opts.charSize + 'px Verdana';
 
 })
+
 
 
 
